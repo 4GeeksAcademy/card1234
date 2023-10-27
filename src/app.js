30fc6abbfd6d7c0781c9ee501cfc19ebebe714bb
Arturo <141342995+amancilla88@users.noreply.github.com>
2023-10-27 23:53:15 +0000

    postcard

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
+// Import the CSS file
 import "./style.css";
-let elementos = ["A", "J", "Q", "K", 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let iconos = ["♦", "♥", "♠", "♣"];
-let random3 = Math.floor(Math.random() * iconos.length);
-let random13 = Math.floor(Math.random() * elementos.length);
 
+// Define arrays for elements and icons
+let elements = ["A", "J", "Q", "K", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let icons = ["♦", "♥", "♠", "♣"];
+
+// Generate random indices for elements and icons
+let random3 = Math.floor(Math.random() * icons.length);
+let random13 = Math.floor(Math.random() * elements.length);
+
+// Get elements by their IDs
 let type1 = document.getElementById("type1");
-type1.textContent = `${iconos[random3]}`;
-
 let number = document.getElementById("number");
-number.textContent = `${elementos[random13]}`;
-
 let type2 = document.getElementById("type2");
-type2.textContent = `${iconos[random3]}`;
 
-if (iconos[random3] === "♥" || iconos[random3] === "♦") {
+// Set the text content
+type1.textContent = icons[random3];
+number.textContent = elements[random13];
+type2.textContent = icons[random3];
+
+// Change the text color to red if the icon is a heart or diamond
+if (icons[random3] === "♥" || icons[random3] === "♦") {
   document.querySelector("#type1").style.color = "red";
   document.querySelector("#type2").style.color = "red";
 }
